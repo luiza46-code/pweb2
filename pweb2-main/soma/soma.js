bf2c3c5dea9640d14fedfffb5c0475bc22d6b9ea
luiza46-code <luizasanclau@gmail.com>
2025-08-06 19:54:19 -0300

    Add files via upload

---
+++ pweb2-main/soma/soma.js
@@ -0,0 +1,10 @@
+
+
+const n1=parseFloat(prompt("Digite um numero"));
+const n2=parseFloat(prompt("Digite um numero"));
+
+var resultado= n1+n2;
+
+console.log("o resultado da soma é: ", resultado);
+
+alert("o resultado da soma é: "+ resultado);
